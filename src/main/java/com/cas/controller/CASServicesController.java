96cee6a1f2041d58e9f346bec0e9be0dcfc74a66
mingtian <505271051@qq.com>
2018-05-07 15:41:28 +0800

    JPA方式持久化票据

--- src/main/java/com/cas/controller/CASServicesController.java
+++ src/main/java/com/cas/controller/CASServicesController.java
@@ -1,15 +1,10 @@
 package com.cas.controller;
 
 
-import org.apereo.cas.services.RegexRegisteredService;
-import org.apereo.cas.services.RegisteredService;
-import org.apereo.cas.services.ReturnAllAttributeReleasePolicy;
-import org.apereo.cas.services.ServicesManager;
+import org.apereo.cas.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.net.URL;
@@ -39,5 +34,28 @@ public class CASServicesController {
         servicesManager.save(service);
         servicesManager.load();
         return "success";
+    }
+
+    @RequestMapping(value = "/addClient/{protocol}/{serviceId}/{id}",method = RequestMethod.GET)
+    public String addClient(@PathVariable("serviceId") String serviceId,@PathVariable("protocol") String protocol
+            ,@PathVariable("id") int id) throws IOException {
+        String url=protocol+"://"+serviceId;
+        RegisteredService svc = servicesManager.findServiceBy(url);
+        if(svc!=null){
+            return "0";//0代表着已存在这个服务，服务是通过正则去匹配的，所以这边建议使用ip或者域名+端口号
+        }
+        String a="^"+url+".*";//匹配以这个url开始的url
+        RegexRegisteredService service=new RegexRegisteredService();
+        ReturnAllAttributeReleasePolicy re=new ReturnAllAttributeReleasePolicy();
+        service.setServiceId(a);
+        service.setId(id);
+        service.setAttributeReleasePolicy(re);
+        //将name统一设置为servicesId
+        service.setName(serviceId);
+        service.setLogoutType(LogoutType.BACK_CHANNEL);
+        service.setLogoutUrl(new URL(url));//这个是为了单点登出而作用的
+        servicesManager.save(service);
+        servicesManager.load();
+        return "1";//添加服务成功
     }
 }
